cf820e13c4ca5fd12e450018ab16c4938dad3508
marvelTtihu <abangpung01@gmail.com>
2026-08-14 13:41:38 +0800

    wip: build experience section layout

--- src/pages/About/section/Experience.jsx
+++ src/pages/About/section/Experience.jsx
@@ -8,23 +8,23 @@ function Experience() {
                 <h2 className="text-center mb-20 text-3xl md:text-5xl font-space">
                     My Experience
                 </h2>
-                <div className='border-l-2 px-4 flex flex-col md:flex-row'>
+                <div className='border-l-2 px-4 flex flex-col md:flex-row text-justify'>
                     <article className='mb-8 md:mb-0 md:mr-4'>
                         <h3 className='text-xl md:text-2xl mb-2 font-space font-bold'>Full-Stack Web Developer</h3>
                         <p className='font-semibold font-inter mb-2'>2025 - Current</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                        <p className='font-inter'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                     </article>
 
                     <article className='mb-8 md:mb-0 md:mr-4 md:border-l-2 md:px-4'>
                         <h3 className='text-xl md:text-2xl mb-2 font-space font-bold'>HTML Developer</h3>
                         <p className='font-semibold font-inter mb-2'>2024-2025</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                        <p className='font-inter'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                     </article>
 
                     <article className='md:border-l-2 md:px-4'>
                         <h3 className='text-xl md:text-2xl mb-2 font-space font-bold'>Python Developer</h3>
                         <p className='font-semibold font-inter mb-2'>2024</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                        <p className='font-inter'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
                     </article>
                 </div>
             </div>
