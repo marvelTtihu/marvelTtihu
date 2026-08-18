dd7996ee2ae8f4cb174ef8ba2d2fa5bd6f2e4970
marvelTtihu <abangpung01@gmail.com>
2026-08-18 10:14:20 +0800

    refactor: change any div to semantic HTML(section/article)

--- src/pages/About/section/Resume.jsx
+++ src/pages/About/section/Resume.jsx
@@ -3,10 +3,10 @@ import { FaEnvelope } from 'react-icons/fa'
 function Resume() {
     return(
         <section className='border-t-2 border-gray-100 px-6 py-24'>
-            <h2 className="mx-auto md:mr-auto md:ml-28 flex items-center text-base gap-2 md:text-lg mb-20 border-2 px-2 py-1 max-w-max rounded-xl font-space"><FaEnvelope /> Resume</h2>
+            <h2 className="ml-4 md:ml-28 flex items-center text-base gap-2 md:text-lg mb-8 border-2 px-2 py-1 max-w-max rounded-xl font-space"><FaEnvelope /> Resume</h2>
             <div className='flex items-center'>
                 <div className="mx-auto px-6 flex flex-col md:flex-row items-start gap-12 md:gap-20">
-                    <div className='flex flex-col items-center gap-8 md:gap-12 text-justify max-w-xl w-full'>
+                    <section className='flex flex-col items-center gap-8 md:gap-12 text-justify max-w-xl w-full'>
                         <h2 className="self-start text-2xl md:text-3xl font-space font-bold">
                             My Experience
                         </h2>
@@ -31,7 +31,7 @@ function Resume() {
                             <p className='font-semibold font-inter mb-2'>2025 - Current</p>
                             <p className='font-inter'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaf</p>
                         </article>
-                    </div>
+                    </section>
                     <div className='flex flex-col items-center gap-8 md:gap-12 text-justify max-w-xl w-full'>
                         <h2 className='self-start font-bold font-space text-2xl md:text-3xl'>My Education</h2>
                         <article className='mb-8 md:mb-0 border-l-2 pl-4 flex-1'>
